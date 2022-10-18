515053a073086175530596daf84354d59628558e
avmirolyubov <avmirolyubov@avito.ru>
2022-10-18 22:19:59 +0300

    oop-style: fix autogenerate file styles; add password generate logic;

--- src/main_window.py
+++ src/main_window.py
@@ -15,89 +15,115 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform, QTextLine)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QPushButton, QSizePolicy,
-    QSlider, QStatusBar, QTextBrowser, QWidget, QLabel)
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QPushButton, QSizePolicy,
+    QSlider, QStatusBar, QTextBrowser, QWidget, QLabel, QCheckBox
+)
 
 
-class UiMainWindow(object):
-    def setup_ui(self, main_window):
+class UiMainWindow:
+    def __init__(self, main_window):
         if not main_window.objectName():
-            main_window.setObjectName(u"MainWindow")
+            main_window.setObjectName("main_window")
         main_window.resize(635, 371)
-        main_window.setStyleSheet(u"background-color: rgb(0, 0, 0)")
-        self.centralwidget = QWidget(main_window)
-        self.centralwidget.setObjectName(u"centralwidget")
-        self.textBrowser = QLabel(self.centralwidget)
-        self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(20, 100, 391, 50))
-        self.textBrowser.setStyleSheet(
-            u"border: 1px solid rgb(255, 255, 255);\n"
+        main_window.setStyleSheet("background-color: rgb(0, 0, 0)")
+
+        self.main_widget = QWidget(main_window)
+        self.main_widget.setObjectName("main_widget")
+
+        self.show_password_field = QLabel(self.main_widget)
+        self.show_password_field.setObjectName("show_password_field")
+        self.show_password_field.setGeometry(QRect(20, 100, 391, 50))
+        self.show_password_field.setStyleSheet(
+            "border: 1px solid rgb(255, 255, 255);\n"
             "border-radius: 10px;\n"
             "color: rgb(255, 255, 255)"
         )
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(420, 100, 61, 51))
-        self.pushButton.setStyleSheet(u"border: 1px solid rgb(255, 255, 255);\n"
-"border-radius: 10px;")
-        self.pushButton_2 = QPushButton(self.centralwidget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(490, 100, 61, 51))
-        self.pushButton_2.setStyleSheet(u"border: 1px solid rgb(255, 255, 255);\n"
-"border-radius: 10px;")
-        self.pushButton_3 = QPushButton(self.centralwidget)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(10, 280, 91, 51))
-        self.pushButton_3.setStyleSheet(u"border: 1px solid rgb(255, 255, 255);\n"
-"border-radius: 10px;\n"
-"color: rgb(255, 255, 255);")
-        self.horizontalSlider = QSlider(self.centralwidget)
-        self.horizontalSlider.setObjectName(u"horizontalSlider")
-        self.horizontalSlider.setGeometry(QRect(30, 220, 351, 22))
-        self.horizontalSlider.setOrientation(Qt.Horizontal)
-        self.textBrowser_2 = QTextBrowser(self.centralwidget)
-        self.textBrowser_2.setObjectName(u"textBrowser_2")
-        self.textBrowser_2.setGeometry(QRect(400, 220, 41, 31))
-        self.textBrowser_2.setStyleSheet(u"border: 1px solid rgb(255, 255, 255);\n"
-"border-radius: 10px;")
-        self.pushButton_7 = QPushButton(self.centralwidget)
-        self.pushButton_7.setObjectName(u"pushButton_7")
-        self.pushButton_7.setGeometry(QRect(120, 280, 91, 51))
-        self.pushButton_7.setStyleSheet(u"border: 1px solid rgb(255, 255, 255);\n"
-"border-radius: 10px;\n"
-"color: rgb(255, 255, 255);\n"
-"")
-        self.pushButton_8 = QPushButton(self.centralwidget)
-        self.pushButton_8.setObjectName(u"pushButton_8")
-        self.pushButton_8.setGeometry(QRect(230, 280, 91, 51))
-        self.pushButton_8.setStyleSheet(u"border: 1px solid rgb(255, 255, 255);\n"
-"border-radius: 10px;\n"
-"color: rgb(255, 255, 255);\n"
-"")
-        self.pushButton_9 = QPushButton(self.centralwidget)
-        self.pushButton_9.setObjectName(u"pushButton_9")
-        self.pushButton_9.setGeometry(QRect(340, 280, 91, 51))
-        self.pushButton_9.setStyleSheet(u"border: 1px solid rgb(255, 255, 255);\n"
-"border-radius: 10px;\n"
-"color: rgb(255, 255, 255);\n"
-"")
-        main_window.setCentralWidget(self.centralwidget)
+
+        self.generate_password_button = QPushButton(self.main_widget)
+        self.generate_password_button.setObjectName("generate_password_button")
+        self.generate_password_button.setGeometry(QRect(420, 100, 61, 51))
+        self.generate_password_button.setStyleSheet(
+            "border: 1px solid rgb(255, 255, 255);\n"
+            "border-radius: 10px;"
+        )
+
+        self.copy_password_to_clipboard = QPushButton(self.main_widget)
+        self.copy_password_to_clipboard.setObjectName("copy_password_to_clipboard")
+        self.copy_password_to_clipboard.setGeometry(QRect(490, 100, 61, 51))
+        self.copy_password_to_clipboard.setStyleSheet(
+            "border: 1px solid rgb(255, 255, 255);\n"
+            "border-radius: 10px;"
+        )
+
+        self.use_lowercase_letters = QPushButton(self.main_widget)
+        self.use_lowercase_letters.setCheckable(True)
+        self.use_lowercase_letters.setObjectName("use_lowercase_letters")
+        self.use_lowercase_letters.setGeometry(QRect(10, 280, 91, 51))
+        self.use_lowercase_letters.setStyleSheet(
+            "border: 1px solid rgb(255, 255, 255);\n"
+            "border-radius: 10px;\n"
+            "color: rgb(255, 255, 255);"
+        )
+
+        self.password_length = QSlider(self.main_widget)
+        self.password_length.setObjectName("password_length")
+        self.password_length.setGeometry(QRect(30, 220, 351, 22))
+        self.password_length.setOrientation(Qt.Horizontal)
+
+        self.password_length_view = QLabel(self.main_widget)
+        self.password_length_view.setObjectName("password_length_view")
+        self.password_length_view.setGeometry(QRect(400, 220, 41, 31))
+        self.password_length_view.setStyleSheet(
+            "border: 1px solid rgb(255, 255, 255);\n"
+            "border-radius: 10px;\n"
+            "color: rgb(255, 255, 255);"
+        )
+
+        self.use_uppercase_letters = QPushButton(self.main_widget)
+        self.use_uppercase_letters.setObjectName("use_uppercase_letters")
+        self.use_uppercase_letters.setGeometry(QRect(120, 280, 91, 51))
+        self.use_uppercase_letters.setStyleSheet(
+            "border: 1px solid rgb(255, 255, 255);\n"
+            "border-radius: 10px;\n"
+            "color: rgb(255, 255, 255);\n"
+        )
+
+        self.use_numbers = QPushButton(self.main_widget)
+        self.use_numbers.setObjectName("use_numbers")
+        self.use_numbers.setGeometry(QRect(230, 280, 91, 51))
+        self.use_numbers.setStyleSheet(
+            "border: 1px solid rgb(255, 255, 255);\n"
+            "border-radius: 10px;\n"
+            "color: rgb(255, 255, 255);\n"
+        )
+
+        self.use_special_chars = QPushButton(self.main_widget)
+        self.use_special_chars.setObjectName("use_special_chars")
+        self.use_special_chars.setGeometry(QRect(340, 280, 91, 51))
+        self.use_special_chars.setStyleSheet(
+            "border: 1px solid rgb(255, 255, 255);\n"
+            "border-radius: 10px;\n"
+            "color: rgb(255, 255, 255);\n"
+        )
+
+        main_window.setCentralWidget(self.main_widget)
         self.statusbar = QStatusBar(main_window)
-        self.statusbar.setObjectName(u"statusbar")
+        self.statusbar.setObjectName("statusbar")
         main_window.setStatusBar(self.statusbar)
 
-        self.retranslateUi(main_window)
+        self.retranslate_ui(main_window)
 
         QMetaObject.connectSlotsByName(main_window)
     # setupUi
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"PasswordGenerator", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Generate password", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Copy password", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"a-z", None))
-        self.pushButton_7.setText(QCoreApplication.translate("MainWindow", u"A-Z", None))
-        self.pushButton_8.setText(QCoreApplication.translate("MainWindow", u"0-9", None))
-        self.pushButton_9.setText(QCoreApplication.translate("MainWindow", u"#$*", None))
+    def retranslate_ui(self, main_window):
+        main_window.setWindowTitle(QCoreApplication.translate("MainWindow", "PasswordGenerator", None))
+        self.generate_password_button.setText(QCoreApplication.translate("MainWindow", "Generate password", None))
+        self.copy_password_to_clipboard.setText(QCoreApplication.translate("MainWindow", "Copy password", None))
+        self.use_lowercase_letters.setText(QCoreApplication.translate("MainWindow", "a-z", None))
+        self.use_uppercase_letters.setText(QCoreApplication.translate("MainWindow", "A-Z", None))
+        self.use_numbers.setText(QCoreApplication.translate("MainWindow", "0-9", None))
+        self.use_special_chars.setText(QCoreApplication.translate("MainWindow", "#$*", None))
     # retranslateUi
 
--- src/password_generator.py
+++ src/password_generator.py
@@ -1,3 +1,6 @@
+from string import ascii_lowercase
+from random import randrange
+
 from PySide6.QtWidgets import QMainWindow
 
 from src.main_window import UiMainWindow
@@ -6,21 +9,40 @@ from src.main_window import UiMainWindow
 class PasswordGenerator(QMainWindow):
     def __init__(self, parent=None):
         super(PasswordGenerator, self).__init__(parent)
-        self.ui = UiMainWindow()
-        self.ui.setup_ui(self)
-        self.ui.pushButton_3.clicked.connect(self.change_value)
-        self.ui.pushButton.clicked.connect(self.generate_password)
-        if self.ui.pushButton_3.isChecked():
-            print("password is strong")
-        else:
-            print("not")
+        self.ui = UiMainWindow(self)
+        self.password_length = 0
+
+        self.ui.use_lowercase_letters.clicked.connect(self.change_value)
+        self.ui.generate_password_button.clicked.connect(self.generate_password)
+        self.ui.password_length.valueChanged[int].connect(self.change_password_length)
+
+        # if self.ui.use_lowercase_letters.isChecked():
+        #     print("password is strong")
+        # else:
+        #     print("not")
 
     def change_value(self):
-        button_style = self.ui.pushButton_3.styleSheet()
+        # button_style = self.ui.use_lowercase_letters.styleSheet()
         # print(button_style, button_style[0])
-        self.ui.pushButton_3.setStyleSheet("background-color: green")
+        if not self.ui.use_lowercase_letters.isChecked():
+            self.ui.use_lowercase_letters.setStyleSheet("background-color: black")
+        else:
+            self.ui.use_lowercase_letters.setStyleSheet("background-color: green")
         # print("Some value")
 
     def generate_password(self):
-        self.ui.textBrowser.setText("New password")
-        print("Button clicked")
+        password = ""
+        print(f"Debug password length: {self.password_length}")
+        print(f"Debug checked a-z status: {self.ui.use_lowercase_letters.isChecked()}")
+        for i in range(self.password_length):
+            if self.ui.use_lowercase_letters.isChecked():
+                password += ascii_lowercase[randrange(len(ascii_lowercase))]
+
+        self.ui.show_password_field.setText(password)
+        # print("Button clicked")
+
+    def change_password_length(self, value):
+        self.password_length = value
+        self.ui.password_length_view.setText(str(value))
+        # print(f"Change value {value}")
+
